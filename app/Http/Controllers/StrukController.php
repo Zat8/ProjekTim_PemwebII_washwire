d60e81f9f83d8daddaee77526af8684e20fed422
naufal <naufalsatrioputra@gmail.com>
2026-06-25 20:00:48 +0700

    fix trash code

--- app/Http/Controllers/StrukController.php
+++ app/Http/Controllers/StrukController.php
@@ -12,7 +12,7 @@ class StrukController extends Controller
         $transaksi->load(['paket', 'kasir']);
 
         $pdf = Pdf::loadView('struk.cetak', ['transaksi' => $transaksi])
-            ->setPaper([0, 0, 226.77, 600], 'portrait'); // ukuran struk kasir (80mm)
+            ->setPaper([0, 0, 226.77, 600], 'portrait');
 
         return $pdf->stream("Struk-{$transaksi->no_nota}.pdf");
     }
